e9ef7bfc6abad49dc1615f68a189e418d9ed1422
anurag4me <anuragmishra5033@gmail.com>
2024-11-26 22:09:08 +0530

    Optimised the code

--- Arrays/Day_1_Second_Largest.cpp
+++ Arrays/Day_1_Second_Largest.cpp
@@ -7,30 +7,19 @@ public:
     int getSecondLargest(vector<int> &arr)
     {
         // Code Here
-        int max1, max2;
-        if (arr[0] > arr[1])
-        {
-            max1 = arr[0];
-            max2 = arr[1];
-        }
-        else
-        {
-            max1 = arr[1];
-            max2 = arr[0];
-        }
-        for (int i = 2; i < arr.size(); i++)
+        int max1 = -1, max2 = -1;
+        for (int i = 0; i < arr.size(); i++)
         {
             if (arr[i] > max1)
             {
                 max2 = max1;
                 max1 = arr[i];
             }
-            else if (arr[i] > max2 && arr[i] < max1)
+            else if (arr[i] > max2 && arr[i] != max1)
             {
                 max2 = arr[i];
             }
         }
-
         return max2;
     }
 };
